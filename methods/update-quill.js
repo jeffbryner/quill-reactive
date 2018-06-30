9d6046b7c77cdc0020938533f1d3ad72e743462a
Jeff Bryner <jeff@jeffbryner.com>
2018-06-30 13:15:36 -0700

    remove verbose logging

--- methods/update-quill.js
+++ methods/update-quill.js
@@ -1,13 +1,13 @@
 if (Meteor.isServer) {
     Meteor.methods({
         'updateQuill': function(collectionName, docId, field, editorContents, editorHTML) {
-            console.log('updateQuill server side called with contents: ',editorContents );
+            //console.log('updateQuill server side called with contents: ',editorContents );
             var collection = Mongo.Collection.get(collectionName);
             var collectionUpdate = { $set: {} };
             collectionUpdate["$set"][field + "Delta"] = editorContents;
             collectionUpdate["$set"][field] = editorHTML;
             collection.update({_id: docId}, collectionUpdate)
-            console.log('collection update', collectionUpdate);
+            //console.log('collection update', collectionUpdate);
         } //end updateQuill
     }) //end Meteor.methods
 };
