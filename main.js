086cecdcb732a68b6cc1cb78e4aa68348ad3e7e9
Jeff Bryner <jeff@jeffbryner.com>
2018-10-07 12:47:09 -0700

    formatting

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 // Client side is created during the template initiation according to meteor load order
 // and the first client in would miss events.
 // So we create the server side here:
+
 if (Meteor.isServer) {
     const streamer = new Meteor.Streamer('quill-reactive-streamer');
     streamer.allowRead('all');  // Everyone can read all events
